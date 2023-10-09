e350e4b3d3643d78c1d7c0b939f3a2bd9de77f34
Bagavathi <sbagavathi2006@gmail.com>
2023-10-08 21:28:34 -0500

    Completed Add Validations and Errors

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,21 +1,38 @@
 package org.launchcode.codingevents.models;
 
+import jakarta.validation.constraints.*;
+
 import java.util.Objects;
 
 public class Event {
-
+    @NotBlank(message = "Name is required.")
+    @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
     private String name;
+    @Size(max = 500, message = "Description too long!")
     private String description;
+    @NotBlank
+    @Email(message = "Invalid email. Try again.")
+    private String contactEmail;
     private int id;
     private static int nextId=1;
 
 
-    public Event(String name, String description) {
+    public Event(String name, String description, String contactEmail) {
         this.id = nextId;
         this.name = name;
         this.description = description;
+        this.contactEmail = contactEmail;
         nextId++;
     }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
     public int getId(){
         return id;
     }
